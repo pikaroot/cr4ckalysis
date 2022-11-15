b97586981a8d4503663297293baa4d3025764bf3
Lim Wei Xun <107750005+pikaroot@users.noreply.github.com>
2022-11-15 17:34:59 +0800

    Add files via upload

---
+++ lib/banner.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# coding: utf-8
+# import pyfiglet
+
+VERSION = "0.0.1"
+AUTHOR  = "pikaroot (David)"
+
+def banner():
+	print(f"Version: {VERSION} Author: {AUTHOR}")
+	banner = """┏━╸┏━┓╻ ╻┏━╸╻┏ ┏━┓╻  ╻ ╻┏━┓╻┏━┓
+┃  ┣┳┛┗━┫┃  ┣┻┓┣━┫┃  ┗┳┛┗━┓┃┗━┓
+┗━╸╹┗╸  ╹┗━╸╹ ╹╹ ╹┗━╸ ╹ ┗━┛╹┗━┛\n"""
+	# banner = pyfiglet.figlet_format("cr4ckalysis", font="future")
+	print(banner)
